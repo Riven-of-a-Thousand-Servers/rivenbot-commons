0df78405fde710d8c564257f783fbf4e861d14ff
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2025-04-23 21:10:11 -0700

    Renamed enums using proper casing

--- pkg/types/enums.go
+++ pkg/types/enums.go
@@ -3,55 +3,55 @@ package types
 type CharacterClass string
 
 const (
-	TITAN   CharacterClass = "TITAN"
-	WARLOCK CharacterClass = "WARLOCK"
-	HUNTER  CharacterClass = "HUNTER"
+	TITAN   CharacterClass = "Titan"
+	WARLOCK CharacterClass = "Warlock"
+	HUNTER  CharacterClass = "Hunter"
 )
 
 type RaidName string
 
 const (
-	SALVATIONS_EDGE     RaidName = "SALVATIONS_EDGE"
-	CROTAS_END          RaidName = "CROTAS_END"
-	ROOT_OF_NIGHTMARES  RaidName = "ROOT_OF_NIGHTMARES"
-	KINGS_FALL          RaidName = "KINGS_FALL"
-	VOW_OF_THE_DISCIPLE RaidName = "VOW_OF_THE_DISCIPLE"
-	VAULT_OF_GLASS      RaidName = "VAULT_OF_GLASS"
-	DEEP_STONE_CRYPT    RaidName = "DEEP_STONE_CRYPT"
-	GARDEN_OF_SALVATION RaidName = "GARDEN_OF_SALVATION"
-	CROWN_OF_SORROW     RaidName = "CROWN_OF_SORROW"
-	LAST_WISH           RaidName = "LAST_WISH"
-	SPIRE_OF_STARS      RaidName = "SPIRE_OF_STARS"
-	EATER_OF_WORLDS     RaidName = "EATER_OF_WORLDS"
-	LEVIATHAN           RaidName = "LEVIATHAN"
-	SCOURGE_OF_THE_PAST RaidName = "SCOURGE_OF_THE_PAST"
+	SALVATIONS_EDGE     RaidName = "Salvation's Edge"
+	CROTAS_END          RaidName = "Crota's End"
+	ROOT_OF_NIGHTMARES  RaidName = "Root of Nightmares"
+	KINGS_FALL          RaidName = "King's Fall"
+	VOW_OF_THE_DISCIPLE RaidName = "Vow of the Disciple"
+	VAULT_OF_GLASS      RaidName = "Vault of Glass"
+	DEEP_STONE_CRYPT    RaidName = "Deep Stone Crypt"
+	GARDEN_OF_SALVATION RaidName = "Garden of Salvation"
+	CROWN_OF_SORROW     RaidName = "Crown of Sorrow"
+	LAST_WISH           RaidName = "Last Wish"
+	SPIRE_OF_STARS      RaidName = "Leviathan, Spire of Stars"
+	EATER_OF_WORLDS     RaidName = "Leviathan, Eater of Worlds"
+	LEVIATHAN           RaidName = "Leviathan"
+	SCOURGE_OF_THE_PAST RaidName = "Scourge of the Past"
 )
 
 type RaidDifficulty string
 
 const (
-	NORMAL         RaidDifficulty = "NORMAL"
-	PRESTIGE       RaidDifficulty = "PRESTIGE"
-	MASTER         RaidDifficulty = "MASTER"
-	GUIDED_GAMES   RaidDifficulty = "GUIDED_GAMES"
-	CHALLENGE_MODE RaidDifficulty = "CHALLENGE_MODE"
+	NORMAL         RaidDifficulty = "Normal"
+	PRESTIGE       RaidDifficulty = "Prestige"
+	MASTER         RaidDifficulty = "Master"
+	GUIDED_GAMES   RaidDifficulty = "Guided Games"
+	CHALLENGE_MODE RaidDifficulty = "Challenge Mode"
 )
 
 type DamageType string
 
 const (
-	KINETIC DamageType = "KINETIC"
-	ARC     DamageType = "ARC"
-	VOID    DamageType = "VOID"
-	SOLAR   DamageType = "SOLAR"
-	STASIS  DamageType = "STASIS"
-	STRAND  DamageType = "STRAND"
+	KINETIC DamageType = "Kinetic"
+	ARC     DamageType = "Arc"
+	VOID    DamageType = "Void"
+	SOLAR   DamageType = "Solar"
+	STASIS  DamageType = "Stasis"
+	STRAND  DamageType = "Strand"
 )
 
 type EquipmentSlot string
 
 const (
-	PRIMARY EquipmentSlot = "PRIMARY"
-	SPECIAL EquipmentSlot = "SPECIAL"
-	HEAVY   EquipmentSlot = "HEAVY"
+	PRIMARY EquipmentSlot = "Primary"
+	SPECIAL EquipmentSlot = "Special"
+	HEAVY   EquipmentSlot = "Heavy"
 )
